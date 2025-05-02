f1a8135c92c5301240b6d1cfddf1f0ffdca6a37a
SaraBarrett <sara.c.mon@gmail.com>
2025-05-02 14:00:10 +0100

    first

---
+++ js/exs.js
@@ -0,0 +1,8 @@
+//ex 17
+
+let myLuckyNumber = 16;
+let myName = 'Sara';
+
+
+
+console.log("o meu nome é "+myName+ " e o meu número preferido é "+myLuckyNumber);
---
+++ js/script.js
@@ -0,0 +1,8 @@
+let myFirstVar = "Olá Mundo!";
+let jsIsCool = true;
+
+console.log(jsIsCool);
+jsIsCool= "aborreci-me dele";
+console.log(jsIsCool);
+jsIsCool= "12";
+console.log(jsIsCool+1);
