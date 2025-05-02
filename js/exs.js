1b05d629e9bf1d31bc9e2152bb6afc6a191870a9
SaraBarrett <sara.c.mon@gmail.com>
2025-05-02 15:27:44 +0100

    string methods

--- js/exs.js
+++ js/exs.js
@@ -1,8 +1,22 @@
 //ex 17
-
 let myLuckyNumber = 16;
 let myName = 'Sara';
 
 
-
+//sintaxe tradicional 
 console.log("o meu nome é "+myName+ " e o meu número preferido é "+myLuckyNumber);
+
+//sintaxe simplificada 
+console.log(`o meu nome é ${myName} e o meu número preferido ${myLuckyNumber}`);
+
+
+//ex pp 28
+const MESSAGE = " TASTE THE RAINBOW ";
+console.log(MESSAGE)
+let whisper = MESSAGE.toLowerCase().trim();
+console.log(whisper)
+
+//pp30
+const WORD = "skateboard";
+let facialHair = WORD.slice(5).replace("o", "e")
+console.log(facialHair);
