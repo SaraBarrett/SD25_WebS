761c83693fbd8d52b8a9f4d7bc698bccd9f1aae4
SaraBarrett <sara.c.mon@gmail.com>
2025-05-12 11:39:26 +0100

    arrays

--- js/exs.js
+++ js/exs.js
@@ -20,3 +20,51 @@ console.log(whisper)
 const WORD = "skateboard";
 let facialHair = WORD.slice(5).replace("o", "e")
 console.log(facialHair);
+
+let myVar;
+
+console.log(myVar)
+
+//ex pp 35
+// let myFirstVar = prompt('Qual é o teu primeiro nome?');
+// let surname = prompt('Qual é o teu primeiro apelido?');
+
+// alert(`Olá ${myFirstVar} ${surname}`);
+// console.log(`Olá ${myFirstVar} ${surname}`);
+
+//ex pp 39
+// let userWeekDay = prompt('que dia da semana é?');
+
+// if(userWeekDay.toLowerCase().trim() == 'sexta'){
+//     console.log('yay, sobrevivemos a mais uma semana');
+// }else{
+//     console.log('força nisso');
+// }
+
+// let userPass = prompt('Qual é a sua pass?');
+
+// if(userPass.length < 6){
+//     console.log('a pass tem que ter pelo menos 6 caracteres');
+// }else{
+//     console.log('pass ok');
+// }
+
+//pp 43
+// let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart', 'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
+// let oldPlanets = planets;
+// planets[3] = 'Marte';
+// planets.pop();
+// planets.unshift('novoPlaneta');
+// console.log(planets)
+
+//pp 55
+for(let i =0; i<6; i++){
+    console.log(i+ ' Da ba dee da ba daa');
+}
+
+for(let i = 25 ; i>=5 ; i-=5){
+    console.log(i);
+}
+
+
+ 
