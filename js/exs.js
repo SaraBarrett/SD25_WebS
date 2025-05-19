e3fbc13401bb5d5460ea5438999679a7cbd7226b
SaraBarrett <sara.c.mon@gmail.com>
2025-05-19 10:39:10 +0100

    arrays & objs

--- js/exs.js
+++ js/exs.js
@@ -1,29 +1,29 @@
 //ex 17
-let myLuckyNumber = 16;
-let myName = 'Sara';
+// let myLuckyNumber = 16;
+// let myName = "Sara";
 
+//sintaxe tradicional
+// console.log(
+//   "o meu nome é " + myName + " e o meu número preferido é " + myLuckyNumber
+// );
 
-//sintaxe tradicional 
-console.log("o meu nome é "+myName+ " e o meu número preferido é "+myLuckyNumber);
-
-//sintaxe simplificada 
-console.log(`o meu nome é ${myName} e o meu número preferido ${myLuckyNumber}`);
-
+//sintaxe simplificada
+// console.log(`o meu nome é ${myName} e o meu número preferido ${myLuckyNumber}`);
 
 //ex pp 28
-const MESSAGE = " TASTE THE RAINBOW ";
-console.log(MESSAGE)
-let whisper = MESSAGE.toLowerCase().trim();
-console.log(whisper)
+// const MESSAGE = " TASTE THE RAINBOW ";
+// console.log(MESSAGE);
+// let whisper = MESSAGE.toLowerCase().trim();
+// console.log(whisper);
 
-//pp30
-const WORD = "skateboard";
-let facialHair = WORD.slice(5).replace("o", "e")
-console.log(facialHair);
+// //pp30
+// const WORD = "skateboard";
+// let facialHair = WORD.slice(5).replace("o", "e");
+// console.log(facialHair);
 
-let myVar;
+// let myVar;
 
-console.log(myVar)
+// console.log(myVar);
 
 //ex pp 35
 // let myFirstVar = prompt('Qual é o teu primeiro nome?');
@@ -58,13 +58,57 @@ console.log(myVar)
 // console.log(planets)
 
 //pp 55
-for(let i =0; i<6; i++){
-    console.log(i+ ' Da ba dee da ba daa');
-}
+// for(let i =0; i<6; i++){
+//     console.log(i+ ' Da ba dee da ba daa');
+// }
 
-for(let i = 25 ; i>=5 ; i-=5){
-    console.log(i);
-}
+// for(let i = 25 ; i>=5 ; i-=5){
+//     console.log(i);
+// }
 
+//pp 58
 
- 
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+// for(let i =0; i < people.length; i++){
+//     console.log(people[i].toUpperCase());
+// }
+
+// for(let name of people){
+//     console.log(name.toUpperCase());
+// }
+
+//pp 62
+// let shoppingList = [];
+// let item = prompt('adicione o item');
+
+// while(item.toLowerCase().trim() != 'fim'){
+// shoppingList.push(item);
+// item = prompt('adicione outro item');
+// }
+
+// for(let element of shoppingList){
+//     console.log("comprar "+element)
+// }
+
+//pp 47
+// const airplaneSeats = [
+//     ["Ruth", "Anthony", "Stevie"],
+//     ["Amelia", 5, "Maya"],
+//     ["Xavier", "Ananya", "Luis"],
+//     ["Luke", null, "Deniz"],
+//     ["Rin", "Sakura", "Francisco"],
+// ];
+// airplaneSeats[3][1] = 'Sara'
+// console.log(airplaneSeats)
+
+//pp 50
+// let product = {
+//     productName: 'crochet',
+//     inStock: true,
+//     price: 1.99,
+//     moeda: '€',
+//     colors: ['vermelho', 'azul', 'verde'] 
+// }
+// product.price = 2.55
+
+// console.log('o produto custa '+product.price +product.moeda+' e a cor é '+product.colors[2])
